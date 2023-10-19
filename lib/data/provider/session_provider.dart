2e2dfb14e7c33db9be76607fe0c112e73dc46027
Jeongyusu <onlys2545@naver.com>
2023-10-19 18:14:23 +0900

    20-10/19 찾 pramstore study

--- lib/data/provider/session_provider.dart
+++ lib/data/provider/session_provider.dart
@@ -1,29 +1,31 @@
-// 1.창고 데이터
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blog/_core/constants/http.dart';
 import 'package:flutter_blog/_core/constants/move.dart';
 import 'package:flutter_blog/data/dto/response_dto.dart';
 import 'package:flutter_blog/data/dto/user_request.dart';
+import 'package:flutter_blog/data/model/user.dart';
+import 'package:flutter_blog/data/repository/user_repository.dart';
 import 'package:flutter_blog/main.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../model/user.dart';
-import '../repository/user_repository.dart';
-
+// 1. 창고 데이터
 class SessionUser {
-  // 1. 화면 context에 접근하는 법
-  final mContext = navigatorKey.currentContext;
-
   User? user;
   String? jwt;
   bool isLogin;
-
   SessionUser({this.user, this.jwt, this.isLogin = false});
+}
+
+// 2. 창고
+class SessionStore extends SessionUser {
+  // 1. 화면 context에 접근하는 법
+  final mContext = navigatorKey.currentContext;
 
   Future<void> join(JoinReqDTO joinReqDTO) async {
-    // 1.통신 코드
+    // 1. 통신 코드
     ResponseDTO responseDTO = await UserRepository().fetchJoin(joinReqDTO);
+
     // 2. 비지니스 로직
     if (responseDTO.code == 1) {
       Navigator.pushNamed(mContext!, Move.loginPage);
@@ -34,19 +36,20 @@ class SessionUser {
   }
 
   Future<void> login(LoginReqDTO loginReqDTO) async {
-    // 1.통신 코드
+    // 1. 통신 코드
     ResponseDTO responseDTO = await UserRepository().fetchLogin(loginReqDTO);
+
     // 2. 비지니스 로직
     if (responseDTO.code == 1) {
       // 1. 세션값 갱신
-      // this.user = responseDTO.data as User;
       this.user = responseDTO.data as User;
       this.jwt = responseDTO.token;
       this.isLogin = true;
-      // 2. 디바이스 JWT 저장
-      await secureStorage.write(key: "jwt", value: responseDTO.token);
-      // 3. 페이지 이동
 
+      // 2. 디바이스에 JWT 저장 (자동 로그인)
+      await secureStorage.write(key: "jwt", value: responseDTO.token);
+
+      // 3. 페이지 이동
       Navigator.pushNamed(mContext!, Move.postListPage);
     } else {
       ScaffoldMessenger.of(mContext!)
@@ -61,14 +64,12 @@ class SessionUser {
     this.user = null;
 
     await secureStorage.delete(key: "jwt");
-    //false를 반환하면 이전의 모든 라우트가 제거된다.
+
     Navigator.pushNamedAndRemoveUntil(mContext!, "/login", (route) => false);
   }
 }
 
-// 2. 창고
-
 // 3. 창고 관리자
-final sessionProvider = Provider<SessionUser>((ref) {
-  return SessionUser();
+final sessionProvider = Provider<SessionStore>((ref) {
+  return SessionStore();
 });
--- lib/data/repository/post_repository.dart
+++ lib/data/repository/post_repository.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_blog/_core/constants/http.dart';
+import 'package:flutter_blog/data/dto/post_request.dart';
 import 'package:flutter_blog/data/dto/response_dto.dart';
+import 'package:logger/logger.dart';
 
 import '../dto/user_request.dart';
 import '../model/post.dart';
@@ -27,7 +29,48 @@ class PostRepository {
       return responseDTO;
     } catch (e) {
       //200이 아니면 catch로 감
-      return ResponseDTO(-1, "중복되는 유저명입니다", null);
+      return ResponseDTO(-1, "게시글 목록 불러오기 실패", null);
+    }
+  }
+
+  Future<ResponseDTO> fetchPost(String jwt, int id) async {
+    try {
+      // 통신
+      Response response = await dio.get("/post/$id",
+          options: Options(headers: {"Authorization": "$jwt"}));
+
+      // 응답 받은 데이터 파싱
+      ResponseDTO responseDTO = ResponseDTO.fromJson(response.data);
+      responseDTO.data = Post.fromJson(responseDTO.data);
+
+      return responseDTO;
+    } catch (e) {
+      return ResponseDTO(-1, "게시글 한건 불러오기 실패", null);
+    }
+  }
+
+  Future<ResponseDTO> savePost(String jwt, PostSaveReqDTO dto) async {
+    try {
+      // 1. 통신
+      final response = await dio.post("/post",
+          data: dto.toJson(),
+          options: Options(headers: {"Authorization": "${jwt}"}));
+      Logger().d(response.data);
+      //2. ResponseDTO 파싱
+      ResponseDTO responseDTO = ResponseDTO.fromJson(response.data);
+      Logger().d(responseDTO.data);
+      // 3.ResponseDTO의 data 파싱
+      // List<dynamic> mapList = responseDTO.data;
+      // List<Post> postList = mapList.map((e) => Post.fromJson(e)).toList();
+      Post post = Post.fromJson(responseDTO.data);
+      //4. 파싱된 데이터를 다시 공통 DTO로 덮어씌우기
+      responseDTO.data = post;
+      // responseDTO.data = User.fromJson(responseDTO.data);
+
+      return responseDTO;
+    } catch (e) {
+      //200이 아니면 catch로 감
+      return ResponseDTO(-1, "게시글 작성 실패", null);
     }
   }
 }
